bd5ab1221c1195b6fc325d4109f513bc32980134
Amrinder Grewal <agrewal@pd-agrewal.local>
2018-02-22 21:14:26 -0500

    added jpa persistance

---
+++ src/main/java/com/springtutorial/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,65 @@
+package com.springtutorial.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by agrewal on 2/22/18.
+ */
+@Entity
+public class Role implements Serializable {
+
+    /** The Serial Version UID for Serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    public Role() {}
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    @Id
+    private int id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return id == role.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
